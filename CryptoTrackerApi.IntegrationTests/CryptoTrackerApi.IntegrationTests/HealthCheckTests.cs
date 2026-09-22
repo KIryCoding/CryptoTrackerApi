e91faaf481ce2fc0e00b4e24780246ec4c89500a
KIryCoding <kyriaki.theocharous@hotmail.com>
2026-09-22 13:24:13 +0300

    Add itegrations tests

---
+++ CryptoTrackerApi.IntegrationTests/CryptoTrackerApi.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CryptoTrackerApi.IntegrationTests;
+
+public class HealthCheckTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+
+    [Fact]
+    public async Task HealthCheck_ReturnsOkAndHealthy()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/health");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Healthy", content);
+    }
+}
